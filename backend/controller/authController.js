e4dbb9b7247b2ededda583044764a8ceda32e067
Harsh gupta <hg9336099029@gmail.com>
2025-03-26 18:21:48 +0530

    feature addition

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -58,17 +58,16 @@ const login = async (req, res) => {
     }
 };
 
-const getuserdetails = async(req,res)=>{
-    try{
-       const user = await User.findById(req.user._id).select('-password');
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+const getuserdetails = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-    res.status(200).json(user); 
-   }
-   catch(error){
-       res.status(500).json({message:error.message});
-   } 
 };
 
 module.exports = { register, login,getuserdetails};
