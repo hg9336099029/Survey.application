dcd74194e18c38b1bc6b6828ef1e6b8acb51c3b5
harsh Gupta <hg9336099029@gmail.com>
2025-10-20 01:01:13 +0530

    update-2.0.4

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'https://survey-application-zeta.vercel.app/',
+  process.env.FRONTEND_URL,
 ];
 
 const corsOptions = {
